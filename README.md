cca3ef370a251e18353368cf7569ee99e1355239
Aislla <aisllaantonia@gmail.com>
2024-03-31 21:01:04 -0300

    Primeiro commit - projeto flutter

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# Projeto Mobile
-O projeto desenvolvido tem como objetivo converter o valor de uma moeda de origem para outra moeda de destino. As moedas utilizadas na conversão são referentes a API: https://www.frankfurter.app/docs/. 
+# projeto_mobile_flutter
 
-Quanto ao funcionamento, é possível digitar o valor a ser convertido e selecionar quais serão as duas moedas utilizadas. Em caso de uma quantidade não ser digitada, será considerado o valor "1" como padrão. 
-Além disso, através da API, é obtido e mostrado qual a data em que a coleta de dados da conversão foi realizada. 
+A new Flutter project.
 
-# Desafios Enfrentados
-Este foi o meu primeiro projeto mobile desenvolvido. Para fazê-lo, precisei aprender a linguagem de programação Dart e o framework Flutter. Além disso, esse também foi meu primeiro projeto utilizando programação orientada a objeto e API. 
+## Getting Started
 
-# Tentativas de Resolução 
-* Procurei usar meus conhecimentos prévios da linguagem C, de estudos de HTML e CSS e da matéria de programação orientada a objeto da Universidade como base para me ajudar a compreender o funcionamento de Dart e Flutter. 
-* Busquei aprender o que são API's e como funcionam as requisições web.
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 103, 58, 183)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Conversor de Moedas'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State <MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _apiData = 0;
+  String dataRetorno = '';
+  Map <String, String> _currencies = {};
+  String moedaSelecionadaBase = '';
+  String moedaSelecionadaDestino = '';
+  final _amountController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCurrencies();
+    fetchData();
+  }
+
+  Future <void> fetchData() async {
+    String amount =_amountController.text.isNotEmpty ? _amountController.text : '1';
+    final response = await http.get(Uri.parse('https://api.frankfurter.app/latest?amount=$amount&from=$moedaSelecionadaBase&to=$moedaSelecionadaDestino'));
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      setState(() {
+        _apiData = jsonData['rates'][moedaSelecionadaDestino];
+        dataRetorno = jsonData['date'];
+      });
+    } else {
+      print('Erro ao carregar dados: ${response.statusCode} ${response.request.toString()}');
+    }
+  }
+
+  Future <void> fetchCurrencies() async {
+    final response =
+        await http.get(Uri.parse('https://api.frankfurter.app/currencies'));
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body) as Map <String, dynamic>;
+      setState(() {
+        _currencies = jsonData.map((key, value) => MapEntry(key, value.toString()));
+        moedaSelecionadaBase = _currencies.keys.first;
+        moedaSelecionadaDestino = _currencies.keys.first;
+      });
+    } else {
+      print('Erro ao carregar dados: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      backgroundColor: const Color.fromARGB(255, 179, 169, 196),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Quantidade',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(.0),
+                  ),
+                  filled: true, 
+                  fillColor:  const Color.fromARGB(255, 215, 209, 219),
+                ),
+              ),
+            ),
+            const SizedBox(
+                height: 30),
+            const Text(
+              'Selecione uma moeda de origem:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            DropdownButton<String>(
+              value: moedaSelecionadaBase,
+              onChanged: (newValue) {
+                setState(() {
+                  moedaSelecionadaBase = newValue!;
+                  fetchData();
+                });
+              },
+              items: _currencies.entries.map((entry) {
+                String currencyCode = entry.key;
+                String currencyName = entry.value;
+                return DropdownMenuItem<String>(
+                  value: currencyCode,
+                  child: Text(
+                    currencyName,
+                    style: const TextStyle(
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                );
+              }).toList(),
+              hint: const Text(
+                'Selecione uma moeda de destino',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              dropdownColor: Colors.white,
+              icon: const Icon(
+                Icons.currency_exchange,
+                color: Colors.deepPurple,
+              ),
+              underline: Container(
+                height: 2,
+                color: const Color.fromARGB(255, 103, 58, 183),
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              'Selecione uma moeda de destino:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            DropdownButton<String>(
+              value: moedaSelecionadaDestino,
+              onChanged: (newValue) {
+                setState(() {
+                  moedaSelecionadaDestino = newValue!;
+                  fetchData();
+                });
+              },
+              items: _currencies.entries.map((entry) {
+                String currencyCode = entry.key;
+                String currencyName = entry.value;
+                return DropdownMenuItem<String>(
+                  value: currencyCode,
+                  child: Text(
+                    currencyName,
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 103, 58, 183),
+                    ),
+                  ),
+                );
+              }).toList(),
+              hint: const Text(
+                'Selecione uma moeda de destino',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 158, 158, 158),
+                ),
+              ),
+              dropdownColor:  const Color.fromARGB(255, 255, 255, 255),
+              icon: const Icon(
+                // Ícone do dropdown
+                Icons.currency_exchange,
+                color: Color.fromARGB(255, 103, 58, 183),
+              ),
+              underline: Container(
+                // Linha sob o dropdown
+                height: 2,
+                color: const Color.fromARGB(255, 103, 58, 183),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Visibility(
+              visible: _apiData != 0 && dataRetorno.isNotEmpty,
+              child: Container(
+                padding:
+                    const EdgeInsets.all(10),
+                margin: const EdgeInsets.only(
+                    top: 10),
+                color:  const Color.fromARGB(255, 238, 238, 238),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      '$_apiData ${_currencies[moedaSelecionadaDestino]}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Color.fromARGB(130, 0, 0, 0),
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      dataRetorno,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(130, 0, 0, 0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
